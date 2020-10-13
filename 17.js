4e0dd1e97cb346884925a18bdda42d4bb967e817
cos-ltpu <ochigazhova@mail.ru>
2020-10-13 12:32:47 +0300

    change 17,20

--- 17.js
+++ 17.js
@@ -1,4 +1,4 @@
-function isObject(obj) {
-    if ((typeof obj == "object") && obj != null) return true;
+module.exports.isObject = function isObject(obj) {
+    if ((typeof obj == "object") & (obj != null)) return true;
     else return false;
 }
--- 20.js
+++ 20.js
@@ -1,3 +1,14 @@
-function Async(arr) {
-    for (let i=0; i!=arr.length; i++) arr[i]().then(result => alert(result), error => alert(error));
+module.exports.Sync = async function Async(arr,str) {
+
+    for (let i = 0; i < arr.length; i++){
+        let p = new Promise( (resolve, reject ) => {
+            arr[i](str)
+            resolve(i+1)
+        });
+        const promisValue = await p;
+        console.log("Step", promisValue)
+
+    }
+
+    return str
 }
